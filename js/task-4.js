da7ee65bada9a52b138064a7da1a15e28cca1a63
AnastAnt <141515437+AnastAnt@users.noreply.github.com>
2024-01-27 13:16:02 +0100

    4

--- js/task-4.js
+++ js/task-4.js
@@ -1,4 +1,7 @@
-
+const getTotalBalanceByGender = (users, gender) =>
+  users
+    .filter((user) => user.gender === gender)
+    .reduce((totalBalance, user) => totalBalance + user.balance, 0);
 
 
 const allUsers = [
